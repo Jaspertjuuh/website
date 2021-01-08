49cc57b91ae13b7f31a5640f7559d156b0bf2468
Jasper <wj.onrust@student.alfa-college.nl>
2021-01-08 16:01:52 +0100

    versie 0.1 dit is een test

---
+++ school-website-main/index.php
@@ -0,0 +1,13 @@
+<html>	
+<head>
+	<title>Jasper Onrust</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="index.css">
+	<link rel="icon" href="favicon.ico">
+</head>
+<body>
+	<?php include("topbar.html") ?>
+</body>
+</html>
